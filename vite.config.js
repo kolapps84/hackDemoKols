dd2841b3cd65cddce1690e7668a75ff360f6d2e5
Kolappan S <kols@microsoft.com>
2025-09-16 23:00:02 -0700

    Fix deployment config and build for hackDemoKols repo

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: './src',
   base: '/hackDemoKols/',
   build: {
-    outDir: '../dist',
+    outDir: 'dist',
     emptyOutDir: true,
   },
   server: {
